d00899708a9b56cf599cadc9ac3ef5440877b3df
Anurag <anuragsinghg99@gmail.com>
2022-06-22 11:11:51 +0530

    Try command

--- src/components/categories/category.js
+++ src/components/categories/category.js
@@ -3,7 +3,14 @@ import "./category.style.scss";
 import categories from "./categorymenu";
 
 const Category = () => {
-  return <Directory categories={categories} />;
+  return (
+    <>
+      <Directory categories={categories} />
+      <div>
+        <h1>This is An ecommerce app</h1>
+      </div>
+    </>
+  );
 };
 
 export default Category;
