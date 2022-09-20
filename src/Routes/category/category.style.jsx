69d8534c563092c6760e22eb94e9b30ca6ea35d8
Anurag <anuragsinghg99@gmail.com>
2022-09-20 13:36:14 +0530

    make responsive shop

--- src/Routes/category/category.style.jsx
+++ src/Routes/category/category.style.jsx
@@ -1,10 +1,15 @@
 import styled from "styled-components";
 
 export const CategoryItemContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  column-gap: 10px;
-  row-gap: 50px;
+  // display: grid;
+  // grid-template-columns: repeat(4, 1fr);
+  // column-gap: 10px;
+  // row-gap: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 2rem;
+  flex-wrap: wrap;
   width: 95%;
   margin: auto;
 `;
